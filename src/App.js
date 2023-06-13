b121161569120791f5c51b311f3456cd14f040f4
Sergio <serarroy@ucm.es>
2023-06-13 17:32:16 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-/*import React, { useEffect, useState } from 'react';
-
-export default function Hangman({duration = 120000}) {
-    const word = "Hangman".toUpperCase();
-    const alphabets = ["A", "B", "C", "D", "E", "F", "G",
-        "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-        "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    const [correctGuesses, setCorrectGuesses] = useState([])
-    const [timeUp, setTimeUp] = useState(false);
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setTimeUp(true);
-        }, duration);
-
-        return () => clearTimeout(timeout);
-    }, [])
-
-
-    const maskedWord = word.split('').map(letter => correctGuesses.includes(letter) ? letter : "_").join(" ");
-    return (
-        <div>
-            <p>{maskedWord}</p>
-            {alphabets.map((alphabet, index) => <button key={index} onClick={() => {
-                if (word.includes(alphabet)) {
-                    setCorrectGuesses([...correctGuesses, alphabet])
-                }
-            }}>{alphabet}</button>)}
-            {timeUp ? 
-            <p>You lost!</p> : 
-            !maskedWord.includes("_") &&  <p>You won!</p>}
-        </div>
-    );
-}*/
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage';
